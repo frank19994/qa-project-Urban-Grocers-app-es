e28f4029d38e00cff452a9190256e2814accb517
Juan Francisco Enrique Aleman Ortegs <franciscoo638@gmail.com>
2025-03-04 16:49:23 -0600

    proyecto sprint 7

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-4babacc2-59fc-4d84-a268-6abbf725a63f.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-b4bc9e26-7c79-4805-a6f2-ff3ac27cf7ba.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
